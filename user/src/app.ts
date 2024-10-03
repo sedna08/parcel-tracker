945c10e438257cec8204f5471fa7353886f25262
sedna08 <ochiamitcham@gmail.com>
2024-10-03 21:27:22 +0800

    Updated data-source and added GET request for all parcel rows

--- user/src/app.ts
+++ user/src/app.ts
@@ -1,5 +1,8 @@
 import * as express from "express";
 import { Express, Request, Response } from "express";
+import { Parcel } from "./entity/Parcel";
+import { Parcel_Actions } from "./entity/Parcel_Action";
+import { AppDataSource } from "./data-source";
 import * as compression from "compression";
 import * as dotenv from "dotenv";
 
@@ -7,10 +10,43 @@ dotenv.config();
 
 const PORT = process.env.POSTGRES_PORT;
 
+// establish database connection
+AppDataSource
+    .initialize()
+    .then(async () => {
+        console.log("Data Source has been initialized!");
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization:", err)
+    });
+
 // create and setup express app
 const app: Express = express();
 app.use(express.json());
 app.use(compression());
+
+// register routes
+
+app.get("/parcels", async (req: Request, res: Response) => {
+    try {
+        const users = await AppDataSource.getRepository(Parcel).find();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// app.get("/parcels/:parcelId", async (req: Request, res: Response) => {
+//     const { parcelId } = req.params;
+//     try {
+//         const parcels = await AppDataSource.getRepository(Parcel).find({
+//             where: { parcelId: parcelId }
+//         });
+//         res.json(parcels);
+//     } catch (error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// });
 
 // start express server
 app.listen(PORT, () => {
--- user/src/data-source.ts
+++ user/src/data-source.ts
@@ -3,7 +3,7 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-export const myDataSource = new DataSource({
+export const AppDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
     port: 5432,
