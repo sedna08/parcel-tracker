c2cc01f4aa621bc169f51af98957af0a1e3c3ea5
sedna08 <ochiamitcham@gmail.com>
2024-10-03 20:35:02 +0800

    Removed user/node_modules

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # PARCEL TRACKER
 
 ## Pre-requisites
-- `install ts-node`
 - `install docker`
-- `install typeORM`
-- `install express`
--- user/src/app.ts
+++ user/src/app.ts
@@ -1,1 +1,18 @@
-console.log("Application is up and running")
+import * as express from "express";
+import { Express, Request, Response } from "express";
+import * as compression from "compression";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.POSTGRES_PORT;
+
+// create and setup express app
+const app: Express = express();
+app.use(express.json());
+app.use(compression());
+
+// start express server
+app.listen(3000, () => {
+    console.log(`Server is running and listening at ${PORT}, Open in browser: http://localhost:3000`);
+})
