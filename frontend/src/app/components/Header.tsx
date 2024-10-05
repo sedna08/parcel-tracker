c2cedc01a83b245cb381d105cae64c35556f8f5a
sedna08 <ochiamitcham@gmail.com>
2024-10-05 20:20:44 +0800

    Created Header Component, added custom colors in tailwin config, and updated homepage

---
+++ frontend/src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="bg-customYellow p-4">
+      <div className="container mx-auto flex justify-center items-center">
+        <h1 className="text-customRed text-2xl font-bold"><b><i>Parcel Tracker</i></b></h1>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -11,6 +11,8 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        customYellow: '#ffc112',
+        customRed: '#ff2222'
       },
     },
   },
