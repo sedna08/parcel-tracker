0c91ac8e3c9578cba494ecce59a7a22a3fba1ebd
sedna08 <ochiamitcham@gmail.com>
2024-10-06 10:49:02 +0800

    Installed axios and updated home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,22 @@
-import Link from 'next/link';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+// import axios from 'axios';
 import Header from './components/Header';
 
 export default function Home() {
+
+    const getParcels = async {
+
+    }
     return (
 
         <div>
             <Header />
 
-            <main className="container mx-auto p-4">
+            <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
+                <div className="space-y-4 w-full max-w-2xl">
+                    <form onSubmit={getParcels} className ="">
+                    </form>
+                </div>
             </main>
             
         </div>
