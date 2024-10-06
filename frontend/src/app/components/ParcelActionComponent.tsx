d05473664c22e2372dd0d837dbe5a53a6a437d79
sedna08 <ochiamitcham@gmail.com>
2024-10-06 18:22:07 +0800

    Updated margin

--- frontend/src/app/components/ParcelActionComponent.tsx
+++ frontend/src/app/components/ParcelActionComponent.tsx
@@ -7,9 +7,9 @@ const ParcelActionComponent: React.FC<{ parcelAction: ParcelActions }> = ({ parc
     const formattedDate = date.toLocaleDateString('en-US')
 
     return (
-        <div className ="container mx-auto flex justify-between items-center rounded-lg p-2 mb-2 hover:bg-gray-100">
-        <div className ="text-lg font-semibold text-gray-800">{formattedDate}</div>
-        <div className ="text-lg font-semibold text-gray-800">{parcelAction.details}</div>
+        <div className ="container mx-auto flex justify-between items-center rounded-lg p-2 hover:bg-gray-100">
+        <div className ="text-lg font-semibold text-gray-800 mr-2">{formattedDate}</div>
+        <div className ="text-lg font-semibold text-gray-800 ml-2">{parcelAction.details}</div>
         </div>
     );
 }
